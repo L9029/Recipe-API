f66ae721a175fcb671de7000ab23165379552d05
L9029 <angeloca03@gmail.com>
2025-08-20 16:25:04 -0400

    Se implementaron los recursos CategoryCollection y CategoryResource para formatear las respuestas de las categorías en la API - Por ultimo se procedio a desarrollar la logica para los metodos index y show del controlador CategoryController que manejaran los endpoints relacionados con las categorias.

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -5,15 +5,21 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Resources\Api\V1\CategoryCollection;
+use App\Http\Resources\Api\V1\CategoryResource;
 
 class CategoryController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Retorna una lista de categorias paginadas.
+     * 
+     * @return \App\Http\Resources\Api\V1\CategoryCollection // Colección de recursos de las categorias
      */
     public function index()
     {
-        //
+        $categories = Category::latest()->paginate(10); // Se optienen las categorias mas recientes paginadas
+
+        return new CategoryCollection($categories);
     }
 
     /**
@@ -25,11 +31,14 @@ class CategoryController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Retorna una categoria específico.
+     * 
+     * @param  \App\Models\Category  $category
+     * @return \App\Http\Resources\V2\PostResource // Recurso que formatea la respuesta de category
      */
     public function show(Category $category)
     {
-        //
+        return new CategoryResource($category);
     }
 
     /**
