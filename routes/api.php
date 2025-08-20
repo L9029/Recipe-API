6f16d8a1999e19aa6fff58fe1bc9ac026b720db1
L9029 <angeloca03@gmail.com>
2025-08-20 15:41:29 -0400

    Se procedieron a crear las rutas de la versión 1 de la API para los endpoints de categories, tags y recipes - Por ultimo se crearon los controladores necesarios para el manejo de los endpoints de la API.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\CategoryController;
+use App\Http\Controllers\Api\V1\TagController;
+use App\Http\Controllers\Api\V1\RecipeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+// Rutas de la API versión 1
+Route::prefix('v1')->group(function () {
+    // Categorias
+    Route::apiResource('categories', CategoryController::class)->only(['index', 'show']);
+
+    // Etiquetas
+    Route::apiResource('tags', TagController::class)->only(['index', 'show']);
+
+    // Recetas
+    Route::apiResource('recipes', RecipeController::class)->only(['index', 'show']);
+});
