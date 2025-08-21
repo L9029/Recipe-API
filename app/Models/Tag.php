c009756a60ab3d852bbbf343316f1fd75e6be864
L9029 <angeloca03@gmail.com>
2025-08-21 16:00:25 -0400

    Se procedio a definir la relación entre Tag y Recipe en el modelo Tag que asocia una etiqueta con muchas recetas.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -9,4 +9,14 @@ class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
+
+    /**
+     * Define la relación entre Tag y Recipe.
+     * Una etiqueta puede estar asociada a muchas recetas (relación muchos a muchos).
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany<Recipe>
+     */
+    public function recipes() {
+        return $this->belongsToMany(Recipe::class);
+    }
 }
